37f0f92be2dc71a119cfc4acdd0d54a2cde9af06
Matt Jones <mattjones315@gmail.com>
2017-02-07 12:42:55 -0800

    Fano Filter Added, Transforms begun

--- R/Filters.R
+++ R/Filters.R
@@ -1,17 +1,35 @@
+## Functions that are used to select genes
 
-applyFilters <- function(data, threshold, nofilter, lean) {
+## Functions here use a variety of criteria to reduce the number
+## of genes to a more manageable size - ideally extracting the
+## genes that are more biologically informative.
+
+
+applyFilters <- function(data, threshold, nofilter, fano) {
+  #'Applies filters to the inputted expression data (may remove rows)
+  #'
+  #'Parameters:
+  #'  data: (data.frame) expression matrix
+  #'  threshold: (int) minimum number of samples gene must be detected in to pass
+  #'  nofilter: (logical) if true, only filter rows that have all identical values
+  #'  fano: (logical) if true, apply the fano filter to the expression matrix
+  #'Returns: (data.frame) filtered expression matrix
+  
   
   if(nofilter) {
   
-    data <- filterGenesNovar(data)
+    data <- filterGenesNovar(data.frame(data))
 
   } else {
 
-    data <- filterGenesThreshold(data, threshold)
-    
-    if (!lean) {
+    if (!fano) {
       
-      print("not lean!")
+      data <- filterGenesThreshold(data.frame(data), threshold)
+      
+    } else {
+      
+      data <- filterGenesFano(data.frame(data))
+      
     }
     
   }
@@ -24,15 +42,77 @@ applyFilters <- function(data, threshold, nofilter, lean) {
 
 # Remove genes with 0 variance
 filterGenesNovar <- function(data) {
+  #' Eliminate genes whose sample variance is equal to 0 (may remove rows); run when --nofilter option 
+  #' is selected
+  #' 
+  #' Parameters:
+  #'  data: (data.frame) expression matrix
+  #' Returns: (data.frame) filtered expression matrix
   
   d <- data.frame(data)
-  return (as.matrix(subset(d, apply(d[,-1], 1, var) != 0)))
+  return (subset(d, apply(d[,-1], 1, var) != 0))
   
 }
 
-filteGenesThreshold <- function(data, threshold) {
-  
+filterGenesThreshold <- function(data, threshold) {
+  #' Filter genes whose values sum to less than some threshold value (may remove rows)
+  #' 
+  #' Parameters:
+  #'  data: (data.frame) expression matrix
+  #'  threshold: (int) threshold value to filter by 
+  #' Returns: (data.frame) filtered expression matrix
+
   d <- data.frame(data)
-  return (as.matrix(subset(d, rowSums(d[,-1]) >= threshold)))
+  return (subset(d, rowSums(d[,-1]) >= threshold))
   
 }
+
+filterGenesFano <- function(data, num_mad=2) {
+  #' Applies the Fano filter to the input data (may remove rows)
+  #' 
+  #' Paramaters:
+  #'  data: (data.frame) expression matrix
+  #'  num_mad: (float) number of median absolute deviations 
+  #' Returns: (data.frame) matrix with logical values, TRUE if gene passes fano filter; else FALSE
+  
+  d <- data.frame(data)
+  
+  mu <- apply(d[,-1], 1, mean)
+  sigma <- apply(d[,-1], 1, sd)
+  
+  
+  aa <- order(mu)
+  mu_sort <- mu[aa]
+  sigma_sort <- sigma[aa]
+  
+  
+  N_QUANTS <- 30
+  m <- floor(length(mu_sort) / N_QUANTS)
+  
+  gene_passes <- rep(0, nrow(d)) == 1
+  
+  for (i in 0:N_QUANTS) {
+    if (i == N_QUANTS - 1) {
+      rr <- seq(from=i*m, to=length(mu_sort))
+    } else {
+      rr <- seq(i*m, (i+1) * m)
+    }
+    
+    mu_quant <- mu_sort[rr]
+    mu_quant[mu_quant==0] = 1
+    sigma_quant <- sigma_sort[rr]
+    fano_quant <- (sigma_quant ** 2) / (mu_quant)
+    mad_quant <- median(abs(fano_quant - median(fano_quant)))
+    gene_passes_quant <- fano_quant > (median(fano_quant) + num_mad * mad_quant)
+    gene_passes_quant_i = which(gene_passes_quant != 0);
+    gene_passes_i <- gene_passes_quant_i + (i * m);
+    gene_passes[gene_passes_i] <- TRUE
+
+  }
+
+  original_ii = order(aa)
+  gene_passes <- gene_passes[original_ii]
+  print(length(which(gene_passes == TRUE)))
+  return(gene_passes)
+}
+
