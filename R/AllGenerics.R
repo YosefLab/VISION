3f46074aedb21e809cf802c39f59693c3c54e63b
Matt Jones <mattjones315@gmail.com>
2017-02-22 12:15:45 -0800

    Added FastProject Class

---
+++ R/AllGenerics.R
@@ -0,0 +1,15 @@
+setGeneric("Analyze", function(object) {
+  standardGeneric("Analyze")
+})
+
+setGeneric("createOutputDirectory", function(object) {
+  standardGeneric("createOutputDirectory")
+})
+
+setGeneric("readExprData", function(object) {
+  standardGeneric("readExprData")
+})
+
+setGeneric("updateExprData", function(object, newData=data.frame()) {
+  standardGeneric("updateExprData")
+})
