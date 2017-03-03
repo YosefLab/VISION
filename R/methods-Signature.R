ae8e3a10013f8c5579db199ed415c97409a04220
Matt Jones <mattjones315@gmail.com>
2017-03-03 08:33:51 -0800

    Added Signature wrapper class

---
+++ R/methods-Signature.R
@@ -0,0 +1,33 @@
+setMethod("initialize", signature(.Object="Signature"),
+          function(.Object, sigDict, name, source, metaData=NULL) {
+            if (missing(sigDict)) {
+              stop("Missing sigDict information.")
+            } else if (missing(name)) {
+              stop("Missing signature name.")
+            } else if (missing(source)) {
+              stop("Missing source file.")
+            }
+            
+            .Object@sigDict = sigDict
+            .Object@name = name
+            .Object@source = source
+            .Object@metaData = metaData
+            
+            return(.Object);
+            
+          }
+)
+
+setMethod("sigEqual", signature(object="Signature"),
+          function(object, compareSig) {
+            if (class(compareSig) != "Signature") {
+              stop("Input compareSig is not a Signature data type.")
+            }
+            
+            if (object@name != compareSig@name) {
+              return(FALSE)
+            }
+            
+            return(object@sigDict == compareSig@sigDict)
+            
+          })
