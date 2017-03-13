1aa92e2c4dffcfa5ea0c0a8185677173fff94964
Matt Jones <mattjones315@gmail.com>
2017-03-12 21:58:42 -0700

    reg pca done

--- R/Projections.R
+++ R/Projections.R
@@ -21,9 +21,36 @@ generateProjections <- function(exprData, filterName="", inputProjections=NULL) 
   if (inputProjections == NULL) {
     inputProjections = c()
   }
+
   
+}
+
+performPCA <- function(data, N=0, variance_proportion=1.0) {
+  #' Performs PCA on data
+  #' 
+  #' Parameters:
+  #'  data: (Num_Features x Num_Samples) matrix
+  #'    Matrix containing data to project into 2D
+  #'  N: int
+  #'    Number of Principle Components to reatin
+  #'  variance_proportion: float
+  #'    Retain top X principal components such taht a total of <variance_proportion> of the 
+  #'    variance is retained  
+  #'  
+  #' Returns:
+  #'  pca_data: (Num_Components x Num_Samples) matrix
+  #'    Data transformed using PCA. Num_Components = Num_Samples
   
+  datat = t(data)
   
+  res <- prcomp(datat, center=TRUE, scale=TRUE)
   
+  if(N == 0) {
+    
+    total_var <- as.matrix(cumsum(res$sdev^2 / sum(res$sdev^2)))
+    last_i <- tail(which(total_var <= variance_proportion), n=1)
+    N <- last_i
+  }
   
+  return (res$x[,1:N])
 }
