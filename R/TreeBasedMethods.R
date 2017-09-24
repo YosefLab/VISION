c6dbd34b0a1bfd315f5a8b0b03f32dfbc2ea4297
talashuach <tal.ashuach@gmail.com>
2017-09-23 21:29:20 -0700

    Documenting Tree methos + moving SPPT
    Moved SPPT functions from projection file to tree file and added some
    documentation.

--- R/TreeBasedMethods.R
+++ R/TreeBasedMethods.R
@@ -1,5 +1,213 @@
 require("igraph")
 
+#' Applies the Simple PPT algorithm onto the expression data.
+#'
+#' @param exprData Expression data -- Num_Genes x Num_Samples
+#' @param numCores Number of cores to use during this analysis
+#' @param nNodes Number of nodes to find. Default is sqrt(N)
+#' @param sigma regularization parameter for soft-assignment of data points to nodes, used as the variance
+#'        of a guassian kernel. If 0, this is estimated automatically
+#' @param gamma graph-level regularization parameter, controlling the tradeoff between the noise-levels
+#'        in the data and the graph smoothness. If 0, this is estimated automatically.
+#' @return Information on the fitten tree
+#'   \itemize {
+#'     \item C spatial positions of the tree nodes in NUM_FEATURES dimensional space
+#'     \item W Unweighted (binary) adjacency matrix of the fitten tree
+#'     \item distMat distance matrix between each tree node to each datapoint
+#'     \item mse the Mean-Squared-Error of the fitten tree
+#'   }
+#' @return Positions of the nodes in NUM_FEATURES dimensional space
+#' @return Binary adjacency matrix of fitted tree
+#' @return Distance matrix between fitted tree nodes and data points
+#' @return Score indicating how strucutred the data is, as the z-score of the data MSE from shuffled MSE
+
+applySimplePPT <- function(exprData, numCores, nNodes_ = round(sqrt(ncol(exprData))), sigma=0, gamma=0) {
+
+  #exprData <- t(exprData)
+
+  MIN_GAMMA <- 1e-5
+  MAX_GAMMA <- 1e5
+  DEF_TOL <- 1e-2
+  DEF_MAX_ITER <- 50
+
+  C <- NULL
+  Wt <- NULL
+
+  if (sigma == 0) {
+    km <- kmeans(t(exprData), centers=round(sqrt(ncol(exprData))), nstart=1, iter.max=50)$centers
+
+    sigma <- mean(apply(as.matrix(sqdist(t(exprData), km)), 1, min))
+  }
+
+  if (gamma == 0) {
+
+    currGamma <- MIN_GAMMA
+    nNodes <- round(log(ncol(exprData)))
+
+    prevMSE <- -Inf
+    minMSE <- Inf
+    minMSEGamma <- MIN_GAMMA
+
+    tr <- fitTree(exprData, nNodes, sigma, currGamma, DEF_TOL, DEF_MAX_ITER)
+    C <- tr$C
+    Wt <- tr$W
+    currMSE <- tr$mse
+
+    while ( ((prevMSE / currMSE) - 1 < 0.05) && currGamma <= MAX_GAMMA) {
+      prevMSE <- currMSE
+      currGamma <- currGamma * 10
+      tr <- fitTree(exprData, nNodes, sigma, currGamma, DEF_TOL, DEF_MAX_ITER)
+      C <- tr$C
+      Wt <- tr$W
+      currMSE <- tr$mse
+      if (currMSE < minMSE) {
+        minMSE <- currMSE
+        minMSEGamma <- currGamma
+      }
+    }
+
+    if (currGamma == MAX_GAMMA) {
+      currGamma <- minGamma
+      tr <- fitTree(exprData, nNodes, sigma, currGamma, DEF_TOL, DEF_MAX_ITER)
+    }
+    minGamma <- currGamma
+
+    while( (currMSE < prevMSE) && ((prevMSE / currMSE) - 1 > 0.05) ) {
+      prevMSE <- currMSE
+      currGamma <- currGamma * 10
+      minGamma <- minGamma * (10^(1/3))
+      tr <- fitTree(exprData, nNodes, sigma, currGamma, DEF_TOL, DEF_MAX_ITER)
+      C <- tr$C
+      Wt <- tr$W
+      currMSE <- tr$mse
+    }
+
+    if (nNodes_ > nNodes) {
+
+      if (nNodes_ != 0) {
+        nNodes <- nNodes_
+      } else {
+        nNodes <- round(sqrt(ncol(exprData)))
+      }
+
+      tr <- fitTree(exprData, nNodes, sigma, currGamma, DEF_TOL, DEF_MAX_ITER)
+      C <- tr$C
+      Wt <- tr$W
+      currMSE <- tr$mse
+
+      # Calculate the degree distribution
+      deg <- colSums(Wt)
+      br <- seq(0, max(1, max(deg)))
+      degDist <- hist(deg, br, plot=F)$counts
+      deg_g2c <- 0
+      if (length(degDist) > 2) {
+        deg_g2c <- sum(degDist[seq(3, length(degDist))])
+      }
+      deg_g2f <- deg_g2c / nNodes
+
+      while ( !(deg_g2c > 0 && (deg_g2f <= 0.1 || deg_g2c < 5)) && (currGamma >= minGamma) ) {
+        currGamma <- currGamma / sqrt(10)
+        tr <- fitTree(exprData, nNodes, sigma, currGamma, DEF_TOL, DEF_MAX_ITER)
+        C <- tr$C
+        Wt <- tr$W
+        currMSE <- tr$mse
+
+        deg <- colSums(Wt)
+        br <- seq(0, max(1, max(deg)))
+        degDist <- hist(deg, br, plot=F)$counts
+        deg_g2c <- 0
+        if (length(degDist) > 2) {
+          deg_g2c <- sum(degDist[seq(3, length(degDist))])
+        }
+        deg_g2f <- deg_g2c / nNodes
+
+      }
+
+    }
+
+    gamma <- currGamma
+
+  }
+
+  tr <- fitTree(exprData, nNodes, sigma, gamma, DEF_TOL, DEF_MAX_ITER)
+  C <- tr$C
+  Wt <- tr$W
+  mse <- tr$mse
+
+
+  return(list(princPnts = C, adjMat = Wt, distMat = sqdist(t(exprData), t(C)), mse = mse))
+}
+
+#' Fit tree using input parameters
+#'
+#' @param expr Data to fit (NUM_GENES x NUM_SAMPLES)
+#' @param nNodes Number of nodes in the fitted tree, default is square-root of number of data points
+#' @param sigma Regularization parameter for soft-assignment of data points to nodes, used as the
+#'              variance of a gaussian kernel. If 0, this is estimated automatically.
+#' @param gamma Graph-level regularization parameter, controlling the tradeoff between the noise-levels
+#'              in the data and the graph smoothness. If 0, this is estimated automatically
+#' @param tol Tolerance to use when fitting the tree
+#' @param maxIter Maximum number of Iterations ot run the algorithm for
+#' @return (list) Tree characteristics:
+#'   \itemize {
+#'     \item C spatial positions of the tree nodes in NUM_FEATURES dimensional space
+#'     \item unweighted (binary) adjacency matrix
+#'     \item the mean-squared error of the tree
+#'   }
+
+fitTree <- function(expr, nNodes, sigma, gamma, tol, maxIter) {
+  km <- kmeans(t(expr), centers=nNodes, nstart=10, iter.max=100)$centers
+  cc_dist <- as.matrix(sqdist(km, km))
+  cx_dist <- as.matrix(sqdist(t(expr), km))
+  prevScore = Inf
+  currScore = -Inf
+  currIter = 0
+
+  while (!(prevScore - currScore < tol) && !(currIter > maxIter)) {
+    currIter <- currIter + 1
+    prevScore <- currScore
+    W <- mst(graph_from_adjacency_matrix(cc_dist, weighted= T, mode="undirected"))
+    Wt <- get.adjacency(W, sparse=FALSE)
+
+    Ptmp <- -(cx_dist / sigma)
+    Psums <- matrix(rep(apply(Ptmp, 1, logSumExp), each=ncol(Ptmp)), nrow=nrow(Ptmp), ncol=ncol(Ptmp), byrow=T)
+    P <- exp(Ptmp - Psums)
+
+    delta <- diag(colSums(P))
+    L <- laplacian_matrix(W)
+    xp <- crossprod(t(expr), P)
+    invg <- as.matrix(solve( ((2 / gamma) * L) + delta))
+    C <- tcrossprod(xp, invg)
+
+    cc_dist <- as.matrix(dist.matrix(t(C)))
+    cx_dist <- as.matrix(sqdist(t(expr), t(C)))
+
+    P <- clipBottom(P, mi=min(P[P>0]))
+    currScore <- sum(Wt * cc_dist) + (gamma * sum(P * ((cx_dist) + (sigma * log(P)))))
+
+  }
+
+  return(list(C = C, W = Wt, mse = getMSE(C, expr)))
+
+}
+
+
+
+#' Calculates the MSE between C and X
+#'
+#' @param C d x m matrix
+#' @param X d x n matrix
+#' @return Mean Squared Error between C and X.
+getMSE <- function(C, X) {
+
+  if (is.na(C) || is.na(X)) {
+    return(NULL)
+  }
+  mse <- mean( apply( as.matrix(sqdist(t(X), t(C))), 1, min))
+  return(mse)
+}
+
+
 
 #' Project the given dataoints onto the tree defined by the vertices (V.pos) and binary adjacency matrix (princAdj)
 #'
@@ -11,14 +219,14 @@ require("igraph")
 #' and has minimal length of orthogonal projection. Data points are projected with truncated orthogonal projection:
 #' point that fall beyond the edge are projected to the closer node.
 #'
-#' @return a list of the following:
-#' {
-#' \item{"spatial"}{The D-dimensional position of the projected data points}
-#' \itam{"edge"}{a Nx2 matrix, were line i has the indices identifying the edge that datapoint i was projected on,
-#' represented as (node a, node b). For consistency and convenience, it is maintained that a < b}
-#' \item{"edge.pos"}{an N-length numeric with values in [0,1], the relative position on the edge of the datapoint.
-#' 0 is node a, 1 is node b, .5 is the exact middle of the edge, etc.}
-#' }
+#' @return (list) projection information:
+#'   \itemize {
+#'     \item{"spatial"}{The D-dimensional position of the projected data points}
+#'     \itam{"edge"}{a Nx2 matrix, were line i has the indices identifying the edge that datapoint i was projected on,
+#'     represented as (node a, node b). For consistency and convenience, it is maintained that a < b}
+#'     \item{"edge.pos"}{an N-length numeric with values in [0,1], the relative position on the edge of the datapoint.
+#'     0 is node a, 1 is node b, .5 is the exact middle of the edge, etc.}
+#'  }
 #'
 #' @examples
 #' X <- matrix(rnorm(200), nrow = 2)
@@ -72,6 +280,14 @@ projectOnTree <- function(data.pnts, V.pos, princAdj) {
               spatial = projections[-c(1:3),]))
 }
 
+#' Calculate distance matrix between all pairs of ponts based on their projection onto the tree
+#'
+#' @param princPnts (D x K numeric) the spatial locations of the principle points (tree vertices)
+#' @param princAdj (K x K logical) adjacency matrix of the principle graph
+#' @param edgeAssoc (2 x N) for each point, the edge it is projected to (represented as (V1,V2), where V1<V2)
+#' @param edgePos (length N, numeric) relative postion on the edge for each point, in range [0,1]
+#'
+#' @return non-negative symmetric matrix in which [i,j] is the tree-based distance between points i, j.
 calculateTreeDistances <- function(princPnts, princAdj, edgeAssoc, edgePos) {
   # get all distances in principle tree
   princAdjW <- sqdist(t(princPnts), t(princPnts)) * princAdj
@@ -83,8 +299,9 @@ calculateTreeDistances <- function(princPnts, princAdj, edgeAssoc, edgePos) {
   edgeToPnts <- apply(princEdges, 2, function(x) { apply(edgeAssoc==x, 2, all) })
 
   distmat <- matrix(rep(NA, NROW(edgeToPnts) ^ 2), NROW(edgeToPnts))
-
   # compute intra-edge distances. Store in list for later calclations and set values in result matrix
+  ## loop contains assignment to external matrix, so apply can't be used. Alternative is to use Reduce on lapply result,
+  ## but memory footprint could be problematic (it's K-choose-2 matrices of size NxN)
   intraDist <- list()
   for (i in 1:NCOL(princEdges)) {
     inEdgeDist <- calcIntraEdgeDistMat(edge.len = nodeDistmat[princEdges[1,i],
@@ -120,12 +337,21 @@ calculateTreeDistances <- function(princPnts, princAdj, edgeAssoc, edgePos) {
   return(pmax(distmat, t(distmat), na.rm = T))
 }
 
+#' Calculate distances between all points on a given edge, including edge vertices
+#' @param edge.len the length of the node
+#' @param edge.pos the relative positions of the points on the edge (in range [0,1]).
+#' @return a distance matrix, where the first and last points are the edge vertices
 calcIntraEdgeDistMat <- function(edge.len, edge.pos) {
   edge.pos <- c(0, edge.pos, 1) * edge.len
   pos.mat <- replicate(length(edge.pos), edge.pos)
   return(abs(pos.mat - t(pos.mat)))
 }
 
+#' Calculate all distances between points on two different edges
+#' @param v1.dist a vectors of all distances between all points on the first edge,
+#' and the vertex of the first edge that is closer to the second edge
+#' @param v1.dist a vectors of all distances between all points on the first edge,
+#' and the vertex of the first edge that is closer to the second edge
 calcInterEdgeDistMat <- function(v1.dist, v2.dist, path.length) {
   # note that input vector must not contain distance to self!
   v1.mat <- replicate(length(v2.dist), v1.dist)
