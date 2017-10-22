0c383925d6efe76ee5e4f0e772965dcebb0d5c30
David DeTomaso <davedeto@gmail.com>
2017-10-22 12:47:20 -0700

    Fixing doc and vignette-building

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Lists of housekeeping genes
+#'
+#' A dataset containing one or more lists of genes to be used as
+#'  housekeeping genes when fitting the FNR curves.
+#'
+#' @format A named list of character vectors
+#' \describe{
+#'   \item{default}{default list of housekeeping genes}
+#' }
+"housekeeping"
