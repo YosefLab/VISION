ba7fe13123d7e9ba7301576580d1da0d8ce8a905
talashuach <tal.ashuach@gmail.com>
2017-11-27 16:49:00 -0800

    Tree cleanup + fixes
    Should fix the dense-tree issue by reducing the number of neighbors the
    tree projection is based on, and fitting the tree to mid-dimension data
    and not full data

--- R/Projections.R
+++ R/Projections.R
@@ -134,7 +134,8 @@ generateProjections <- function(expr, weights, filterName="",
 #' Genrate projections based on a tree structure learned in high dimensonal space
 #'
 #' @importFrom stats quantile
-#' @param expr the expressionSet object
+#' @param data the data to fit the tree to. Should be lower dimensional than
+#' full data.
 #' @param filterName the filtered data to use
 #' @param inputProjections a list of Projection objects. For each Projection, a
 #' corresponding TreeProjection will be created in which the scores are based
@@ -149,62 +150,23 @@ generateProjections <- function(expr, weights, filterName="",
 #'     \item treeScore a score representing the singificance of the fitten tree
 #'     return(list(projections = output, treeScore = hdTree$zscore))
 #' }
-generateTreeProjections <- function(expr, filterName="",
+generateTreeProjections <- function(data, filterName="",
                                     inputProjections, permMats = NULL,
                                     BPPARAM = bpparam()) {
 
-
-    if (filterName == "novar") {
-    exprData <- expr@noVarFilter
-    } else if (filterName == "threshold") {
-    exprData <- expr@thresholdFilter
-    } else if (filterName == "fano") {
-    exprData <- expr@fanoFilter
-    } else if (filterName == "") {
-    exprData <- expr@data
-    } else {
-    stop("FilterName not recognized: ", filterName)
-    }
-
-    t <- Sys.time()
-    timingList <- (t - t)
-    timingNames <- c("Start")
-
-    hdTree <- applySimplePPT(exprData, permExprData = permMats)
-    hdProj <- TreeProjection(name = "PPT", pData = exprData,
+    hdTree <- applySimplePPT(data, permExprData = permMats)
+    hdProj <- TreeProjection(name = "PPT", pData = data,
                                 vData = hdTree$princPnts, adjMat = hdTree$adjMat)
 
-    ncls <- apply(hdTree$distMat, 1, which.min)
-    pptNeighborhood <- findNeighbors(exprData, ##why was this fullPCA here?
-                                    hdTree$princPnts,
-                                    NCOL(exprData) / NCOL(hdTree$princPnts),
-                                    BPPARAM)
-
-    timingList <- rbind(timingList, c(difftime(Sys.time(), t, units="sec")))
-    timingNames <- c(timingNames, "PPT")
-    rownames(timingList) <- timingNames
-
-    # Readjust coordinates of PPT
-    c <- t(hdTree$princPnts[c(1,2),])
-    coord <- as.matrix(apply(c, 2, function(x) return(x - mean(x))))
-    r <- apply(coord, 1, function(x) sum(x^2))^(0.5)
-    r90 <- quantile(r, c(0.9))[[1]]
-    if (r90 > 0) {
-    coord <- coord / r90
-    }
-    coord <- t(coord)
-    hdTree$princPnts <- coord
+    pptNeighborhood <- findNeighbors(data, hdTree$princPnts, 5, BPPARAM)
 
     output <- list()
     output[[hdProj@name]] <- hdProj
 
-    # Reposition tree node coordinates in nondimensional space
     for (proj in inputProjections) {
-    new_coords <- vapply(pptNeighborhood, function(n)  {
-        centroid <- proj@pData[,n$index] %*% t(n$dist / sum(n$dist))
-        # n_vals <- proj@pData[,n$index] * rep(n$dist / sum(n$dist), rep(nrow(proj@pData), length(n$dist)))
-        # centroid <- apply(n_vals, 1, mean)
-        return(centroid)
+        new_coords <- vapply(pptNeighborhood, function(n)  {
+            centroid <- proj@pData[,n$index] %*% t(n$dist / sum(n$dist))
+            return(centroid)
     }, c(0.0,0.0))
     treeProj = TreeProjection(name = proj@name, pData = proj@pData,
                                 vData = new_coords, adjMat = hdTree$adjMat)
