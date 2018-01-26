ffce2e3a2a81aab0797534bd056f2cea59747d40
David DeTomaso <davedeto@gmail.com>
2018-01-25 17:50:19 -0800

    Add test for signatures

--- tests/testthat/test_signatures.R
+++ tests/testthat/test_signatures.R
@@ -86,3 +86,90 @@ test_that("Naive Sig Eval is same as Weighted Sig Eval with all weights 1", {
   }
   
 })
+
+test_that("Weighted signature scores are correct", {
+    expr <- c(
+              1, 2.0,
+              2, 2,
+              5, 4
+              )
+    expr <- matrix(expr, nrow = 3, byrow = TRUE)
+    rownames(expr) <- c("A", "B", "C")
+    colnames(expr) <- c("s1", "s2")
+
+    weights <- c(
+              .5, .3,
+              .1, .5,
+              .3, .6
+              )
+    weights <- matrix(weights, nrow = 3, byrow = TRUE)
+    rownames(weights) <- rownames(expr)
+    colnames(weights) <- colnames(expr)
+
+    sig1 <- Signature(sigDict = c("a" = 1, "b" = 1), name = "sig1",
+                      source = "", metaData = "", isPrecomputed = FALSE,
+                      isFactor = FALSE, cluster = 0)
+
+    sig2 <- Signature(sigDict = c("b" = 1, "c" = -1), name = "sig2",
+                      source = "", metaData = "", isPrecomputed = FALSE,
+                      isFactor = FALSE, cluster = 0)
+
+    sigs <- list(sig1 = sig1, sig2 = sig2)
+
+    sig1scores <- singleSigEval(sig1, "weighted_avg", expr,
+                  weights, 1)
+
+    sig2scores <- singleSigEval(sig2, "weighted_avg", expr,
+                  weights, 1)
+
+    expected_sig1_scores <- c(s1 = (1 * .5 + 2 * .1) / (.5 + .1),
+                              s2 = (2 * .3 + 2 * .5) / (.3 + .5))
+    expect_equal(sig1scores@scores, expected_sig1_scores)
+    expect_equal(sig1scores@name, sig1@name)
+
+    expected_sig2_scores <- c(s1 = (2 * .1 - 5 * .3) / (.1 + .3),
+                              s2 = (2 * .5 - 4 * .6) / (.5 + .6))
+    expect_equal(sig2scores@scores, expected_sig2_scores)
+    expect_equal(sig2scores@name, sig2@name)
+
+})
+
+test_that("Unweighted signature scores are correct", {
+    expr <- c(
+              1, 2.0,
+              2, 2,
+              5, 4
+              )
+    expr <- matrix(expr, nrow = 3, byrow = TRUE)
+    rownames(expr) <- c("A", "B", "C")
+    colnames(expr) <- c("s1", "s2")
+
+    weights <- matrix(1.0, nrow = nrow(expr), ncol = ncol(expr))
+    rownames(weights) <- rownames(expr)
+    colnames(weights) <- colnames(expr)
+
+    sig1 <- Signature(sigDict = c("a" = 1, "b" = 1), name = "sig1",
+                      source = "", metaData = "", isPrecomputed = FALSE,
+                      isFactor = FALSE, cluster = 0)
+
+    sig2 <- Signature(sigDict = c("b" = 1, "c" = -1), name = "sig2",
+                      source = "", metaData = "", isPrecomputed = FALSE,
+                      isFactor = FALSE, cluster = 0)
+
+    sigs <- list(sig1 = sig1, sig2 = sig2)
+
+    sig1scores <- singleSigEval(sig1, "naive", expr,
+                  weights, 1)
+
+    sig2scores <- singleSigEval(sig2, "naive", expr,
+                  weights, 1)
+
+    expected_sig1_scores <- c(s1 = 1.5, s2 = 2.0)
+    expect_equal(sig1scores@scores, expected_sig1_scores)
+    expect_equal(sig1scores@name, sig1@name)
+
+    expected_sig2_scores <- c(s1 = -3.0, s2 = -2.0) / 2
+    expect_equal(sig2scores@scores, expected_sig2_scores)
+    expect_equal(sig2scores@name, sig2@name)
+
+})
