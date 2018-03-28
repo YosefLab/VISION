582252c4c3a109b4a3b02452043d6059681187c5
David DeTomaso <davedeto@gmail.com>
2018-03-28 12:43:22 -0700

    Fix RcppExports
    Signed-off-by: David DeTomaso <davedeto@gmail.com>

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,69 +5,6 @@
 
 using namespace Rcpp;
 
-// geary
-double geary(NumericVector X, NumericMatrix W);
-RcppExport SEXP _FastProjectR_geary(SEXP XSEXP, SEXP WSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type X(XSEXP);
-    Rcpp::traits::input_parameter< NumericMatrix >::type W(WSEXP);
-    rcpp_result_gen = Rcpp::wrap(geary(X, W));
-    return rcpp_result_gen;
-END_RCPP
-}
-// geary_sparse
-double geary_sparse(NumericVector X, NumericMatrix ind, NumericMatrix W);
-RcppExport SEXP _FastProjectR_geary_sparse(SEXP XSEXP, SEXP indSEXP, SEXP WSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type X(XSEXP);
-    Rcpp::traits::input_parameter< NumericMatrix >::type ind(indSEXP);
-    Rcpp::traits::input_parameter< NumericMatrix >::type W(WSEXP);
-    rcpp_result_gen = Rcpp::wrap(geary_sparse(X, ind, W));
-    return rcpp_result_gen;
-END_RCPP
-}
-// geary_sparse_local
-NumericVector geary_sparse_local(NumericVector X, NumericMatrix ind, NumericMatrix W);
-RcppExport SEXP _FastProjectR_geary_sparse_local(SEXP XSEXP, SEXP indSEXP, SEXP WSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type X(XSEXP);
-    Rcpp::traits::input_parameter< NumericMatrix >::type ind(indSEXP);
-    Rcpp::traits::input_parameter< NumericMatrix >::type W(WSEXP);
-    rcpp_result_gen = Rcpp::wrap(geary_sparse_local(X, ind, W));
-    return rcpp_result_gen;
-END_RCPP
-}
-// geary_all
-NumericVector geary_all(NumericMatrix X, NumericMatrix W);
-RcppExport SEXP _FastProjectR_geary_all(SEXP XSEXP, SEXP WSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericMatrix >::type X(XSEXP);
-    Rcpp::traits::input_parameter< NumericMatrix >::type W(WSEXP);
-    rcpp_result_gen = Rcpp::wrap(geary_all(X, W));
-    return rcpp_result_gen;
-END_RCPP
-}
-// geary_sparse_all
-NumericVector geary_sparse_all(NumericMatrix X, NumericMatrix ind, NumericMatrix W);
-RcppExport SEXP _FastProjectR_geary_sparse_all(SEXP XSEXP, SEXP indSEXP, SEXP WSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericMatrix >::type X(XSEXP);
-    Rcpp::traits::input_parameter< NumericMatrix >::type ind(indSEXP);
-    Rcpp::traits::input_parameter< NumericMatrix >::type W(WSEXP);
-    rcpp_result_gen = Rcpp::wrap(geary_sparse_all(X, ind, W));
-    return rcpp_result_gen;
-END_RCPP
-}
 // ball_tree_vector_knn
 List ball_tree_vector_knn(NumericMatrix X, NumericVector Y, int K, int n_threads);
 RcppExport SEXP _FastProjectR_ball_tree_vector_knn(SEXP XSEXP, SEXP YSEXP, SEXP KSEXP, SEXP n_threadsSEXP) {
@@ -132,11 +69,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_FastProjectR_geary", (DL_FUNC) &_FastProjectR_geary, 2},
-    {"_FastProjectR_geary_sparse", (DL_FUNC) &_FastProjectR_geary_sparse, 3},
-    {"_FastProjectR_geary_sparse_local", (DL_FUNC) &_FastProjectR_geary_sparse_local, 3},
-    {"_FastProjectR_geary_all", (DL_FUNC) &_FastProjectR_geary_all, 2},
-    {"_FastProjectR_geary_sparse_all", (DL_FUNC) &_FastProjectR_geary_sparse_all, 3},
     {"_FastProjectR_ball_tree_vector_knn", (DL_FUNC) &_FastProjectR_ball_tree_vector_knn, 4},
     {"_FastProjectR_ball_tree_knn", (DL_FUNC) &_FastProjectR_ball_tree_knn, 3},
     {"_FastProjectR_load_in_knn", (DL_FUNC) &_FastProjectR_load_in_knn, 2},
