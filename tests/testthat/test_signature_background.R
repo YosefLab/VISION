02eff73c5813663c6eac754ff25826d15a6716e1
David DeTomaso <davedeto@gmail.com>
2019-08-13 14:23:41 -0700

    Fix tests

--- tests/testthat/test_signature_background.R
+++ tests/testthat/test_signature_background.R
@@ -11,46 +11,53 @@ test_that("Can generate random background signatures", {
 
   # Mock the weights
   ed <- object@exprData
-  weights <- matrix(
-                    rep(1, length(ed)),
-                    nrow=nrow(ed)
-                    )
-  rownames(weights) <- rownames(ed)
-  colnames(weights) <- colnames(ed)
-  object@weights <- weights
 
-  out <- calculateSignatureBackground(object, 10)
-  backgroundSigGroups <- out[[1]]
-  sigAssignments <- out[[2]]
+  out <- generatePermutationNull(object@exprData, object@sigData, 10)
+  randomSigs <- out$randomSigs
+  sigAssignments <- out$sigAssignments
+  randomSigAssignments <- out$randomSigAssignments
 
-  expect_is(backgroundSigGroups, "list")
-  expect_gt(length(backgroundSigGroups), 0)
-
-  backgroundSigs <- backgroundSigGroups[[1]]
-
-  expect_is(backgroundSigs, "matrix")
-  expect_gt(ncol(backgroundSigs), 0)
-
-  expect_equal(ncol(backgroundSigs), 10)
-  expect_equal(nrow(backgroundSigs), ncol(object@exprData))
+  expect_is(randomSigs, "list")
+  expect_gt(length(randomSigs), 0)
 
   expect_is(sigAssignments, "factor")
   expect_equal(length(sigAssignments), length(object@sigData))
+  expect_equal(
+      length(levels(sigAssignments)),
+      length(randomSigs) / 10
+  )
+
+  expect_is(randomSigAssignments, "factor")
+  expect_equal(length(randomSigAssignments), length(randomSigs))
+  expect_equal(
+      length(levels(randomSigAssignments)),
+      length(randomSigs) / 10
+  )
+
 
   # Test this file too - has more signatures
   sig_file <- "test_data/published_signatures/h.all.v5.2.symbols.gmt"
 
-  out <- calculateSignatureBackground(object, 10)
-  backgroundSigGroups <- out[[1]]
-  sigAssignments <- out[[2]]
+  out <- generatePermutationNull(object@exprData, object@sigData, 10)
+  randomSigs <- out$randomSigs
+  sigAssignments <- out$sigAssignments
+  randomSigAssignments <- out$randomSigAssignments
 
-  expect_is(backgroundSigGroups, "list")
-  expect_gt(length(backgroundSigGroups), 0)
+  expect_is(randomSigs, "list")
+  expect_gt(length(randomSigs), 0)
 
-  backgroundSigs <- backgroundSigGroups[[1]]
+  expect_is(sigAssignments, "factor")
+  expect_equal(length(sigAssignments), length(object@sigData))
+  expect_equal(
+      length(levels(sigAssignments)),
+      length(randomSigs) / 10
+  )
 
-  expect_is(backgroundSigs, "matrix")
-  expect_equal(ncol(backgroundSigs), 10)
-  expect_equal(nrow(backgroundSigs), ncol(object@exprData))
+  expect_is(randomSigAssignments, "factor")
+  expect_equal(length(randomSigAssignments), length(randomSigs))
+  expect_equal(
+      length(levels(randomSigAssignments)),
+      length(randomSigs) / 10
+  )
 
 })
